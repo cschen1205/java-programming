85441f91e7bb7a42ff89ed34c674820b1908a9ed
Caishun Chen <cschen1205@gmail.com>
2017-06-27 11:06:24 +0800

    chapter 03 started

---
+++ src/main/java/com/github/cschen1205/jp/chap3/Q02.java
@@ -0,0 +1,43 @@
+package com.github.cschen1205.jp.chap3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by memeanalytics on 27/6/17.
+ * How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element? Push, pop and min should all operate in O(1) time
+ */
+public class Q02 {
+    public static class StackWithMin<T extends Comparable<T>> extends Stack<T> {
+        private Stack<T> s2 = new Stack<>();
+        @Override
+        public T push(T item) {
+            if(min().compareTo(item) > 0){
+                s2.push(item);
+            }
+            return super.push(item);
+        }
+
+        public T min() {
+            if(s2.isEmpty()) return null;
+            return s2.peek();
+        }
+
+        @Override
+        public synchronized T pop() {
+
+
+
+            if(!isEmpty()) {
+                T val = peek();
+                if(val.equals(min())) {
+                    s2.pop();
+                }
+            }
+
+
+            return super.pop();
+        }
+    }
+}
