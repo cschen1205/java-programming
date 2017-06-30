a5e9bf4521575d112e9eb6d8f077a7da2013a8fd
Caishun Chen <cschen1205@gmail.com>
2017-06-30 17:10:23 +0800

    find the prev and next numbers which have the same number of one and zero bits

--- src/main/java/com/github/cschen1205/jp/chap05/Q03.java
+++ src/main/java/com/github/cschen1205/jp/chap05/Q03.java
@@ -17,4 +17,67 @@ public class Q03 {
         }
     }
 
+    public static int getNext(int n) {
+        int index = 0;
+        int oneCounts = 0;
+
+        // find first one
+        while(!getBit(n,index++)) {
+
+        }
+
+        // turn on next zero
+        while(getBit(n, index++)) {
+            oneCounts++;
+        }
+        n = setBit(n,index, true);
+
+        // tunr off prev one
+        index--;
+        n = setBit(n, index, false);
+        oneCounts--;
+
+        // set zeroes
+        for(int i=index-1; i >= oneCounts; i--) {
+            n = setBit(n, i, false);
+        }
+
+        // set ones
+        for(int i= oneCounts-1; i >= 0; i--) {
+            n = setBit(n, i, true);
+        }
+
+        return n;
+
+
+    }
+
+    public static int getPrev(int n) {
+        int index = 0;
+        int countZeroes = 0;
+        while(getBit(n, index)) {
+            index++;
+        }
+        while(!getBit(n, index++)) {
+            countZeroes++;
+        }
+        n = setBit(n, index, false);
+
+        index--;
+        n = setBit(n, index, true);
+        countZeroes--;
+
+        // set ones
+        for(int i = index-1; i >= countZeroes; --i) {
+            n = setBit(n, i, true);
+        }
+
+        // set zeroes
+        for(int i=countZeroes-1; i >= 0; --i) {
+            n = setBit(n, i, false);
+        }
+
+        return n;
+    }
+
 }
